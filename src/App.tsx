0284694ec1fc41bd7af056796824f90e5065ecab
Arshal Abbas <arshalabbas123@gmail.com>
2024-11-25 14:28:37 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import NavBar from "./components/shared/Navbar";
+
+const App = () => {
+  return (
+    <main>
+      <NavBar />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/shared/Navbar/index.tsx
@@ -0,0 +1,12 @@
+import "./Navbar.scss";
+
+const NavBar = () => {
+  return (
+    <nav>
+      {/* Top Belt */}
+      <div className="nav-top-belt"></div>
+    </nav>
+  );
+};
+
+export default NavBar;
