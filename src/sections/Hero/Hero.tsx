aa31328da9b9dc6fc4d7d5c1c792a46fa046b685
Arshal Abbas <arshalabbas123@gmail.com>
2024-11-27 10:25:37 +0530

    Fixed the nav sizing

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -5,23 +5,20 @@ import "./Hero.scss";
 const Hero = () => {
   return (
     <div className="layout">
-      <div
-        className="strip-container
-    "
-      >
+      <div className="strip-container">
         <div className="inner-container">
           <span>
-            You are on amazon.com. You can also shop Amazon India for millions
-            of products with fast local delivery.
+            You are on amazon.com. You can also shop on Amazon India for
+            millions of products with fast local delivery.{" "}
           </span>
-          <a href="#"> Click here to go to amazon.in</a>
+          <a href="#">Click here to go to amazon.in</a>
         </div>
       </div>
 
       <div className="grid-layout">
         <GridCard />
-        <ProductCard />
         <GridCard />
+        <ProductCard />
         <GridCard />
       </div>
     </div>
