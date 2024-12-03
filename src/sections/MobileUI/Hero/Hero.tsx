911a2d323400798a8202215098e2fb64e4951921
Arshal Abbas <arshalabbas123@gmail.com>
2024-12-03 14:15:58 +0530

    Initialised the carousel in the mobile ui

---
+++ src/sections/MobileUI/Hero/Hero.tsx
@@ -0,0 +1,16 @@
+import "./Hero.scss";
+
+const Hero = () => {
+  return (
+    <div>
+      <div className="carousel-container">
+        <div className="inner-container">
+          {/* Carousel List */}
+          <ol className="carousel-list"></ol>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
